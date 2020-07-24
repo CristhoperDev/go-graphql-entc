b9876f687cfaae9ff12aae74a4b53f4c6348a563
Cristhoper Alvarez <acristhoper25@gmail.com>
2020-07-24 12:15:34 -0500

    refactor: using viper env variable (#6)

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,11 +3,12 @@ package database
 import (
 	"github.com/CristhoperDev/go-graphql-entc/internal/provider/entc"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func Open() (*entc.Client, error) {
-	client, err := entc.Open("mysql", "root:root@tcp(localhost:3306)/ent_graphql?parseTime=true")
+	client, err := entc.Open(viper.GetString("DATABASE_DRIVER"), viper.GetString("DATABASE_URL"))
 	if err != nil {
 		log.Fatalf("failed connecting to mysql: %v", err)
 	}
